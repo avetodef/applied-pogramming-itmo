da2b980f3c4e18b2b5c27f894f2df15bdb9a5178
avetodef <sofiafedoteva@gmail.com>
2024-03-16 12:36:46 +0300

    я пишу псж

---
+++ lab1/server/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, VARCHAR, Column, String
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column
+
+DATABASE_URL = "postgresql://postgres:lterm54201@localhost:5432/applied_prog_lab_1"
+engine = create_engine(DATABASE_URL)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    username: Mapped[str] = mapped_column(String(50), primary_key = True)
+    password: Mapped[str] = mapped_column(String(50))
--- lab1/server/main.py
+++ lab1/server/main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI, UploadFile, Depends
+from fastapi import FastAPI, UploadFile, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 from starlette import status
 from pydantic import BaseModel
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Float, VARCHAR, Time, Date
-from databases import Database
+
+from .db import get_session, User
 
 app = FastAPI()
 
@@ -18,42 +17,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-DATABASE_URL = "postgresql://postgres:lterm54201@localhost:5432/applied_prog_lab_1"
-database = Database(DATABASE_URL)
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-metadata = MetaData()
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    username = Column(VARCHAR,primary_key=True,nullable=False)
-    user_password = Column(VARCHAR,nullable=False)
-
-
-Base.metadata.create_all(bind=engine)
-
 
 class LoginData(BaseModel):
     login: str
     password: str
-
-
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-@app.get("/users/")
-async def get_users():
-    query = User.__table__.select()
-    return await database.fetch_all(query)
 
 
 @app.post('/upload', status_code=status.HTTP_200_OK)
@@ -61,8 +28,13 @@ def upload_file(file: UploadFile):
     print(file.file.read())
     return file.filename
 
+
 @app.post('/login', status_code=status.HTTP_200_OK)
 def login(data: LoginData) -> LoginData:
     print(data.login, data.password)
     return data
 
+
+@app.get("/users")
+def example(session: Session = Depends(get_session)):
+    return session.execute(select(User)).all()
